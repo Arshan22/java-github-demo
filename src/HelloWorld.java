9cc3f3adadd76d7f0d5dbdb05ff93931eeef5f73
unknown <arshantilaks22@gmail.com>
2025-06-02 14:13:22 +0530

    added Tilakshan

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(srting[]args) {
         System.out.println('Hello from GitHub and Java!');
+        System.out.println("This code was written by Tilakshan")
     }
 }
